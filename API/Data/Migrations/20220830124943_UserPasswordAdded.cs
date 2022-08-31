7755cf5c88918a95dfa411ec4784b68883889529
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-08-31 15:49:26 +0200

    Adding simple Authentication

---
+++ API/Data/Migrations/20220830124943_UserPasswordAdded.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    public partial class UserPasswordAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MyProperty",
+                table: "Users");
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "PasswordHash",
+                table: "Users",
+                type: "BLOB",
+                nullable: true);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "PasswordSolt",
+                table: "Users",
+                type: "BLOB",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PasswordHash",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "PasswordSolt",
+                table: "Users");
+
+            migrationBuilder.AddColumn<int>(
+                name: "MyProperty",
+                table: "Users",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
